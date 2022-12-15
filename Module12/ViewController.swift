9f17234091eb1bd3206bf3c7007d633b1964cdd0
Andrey Zharov <98423145+kotttobormottt@users.noreply.github.com>
2022-12-15 10:17:03 +0300

    Initial Commit

---
+++ Module12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module12
+//
+//  Created by Andrey Zharov on 15.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
